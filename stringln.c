dca85e14c64b39f6924ac61f1ffbdda58c63407d
yc1797 <yc1797@nyu.edu>
2016-02-09 15:25:54 -0500

    Create stringln.c

---
+++ stringln.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+#include <string>
+
+int stringLength(string a)
+{
+  int count = 0;
+  for(int i = 0; a[i] != "\0")
+  {
+    count++;
+    i++;
+  }
+
+  return count;
+}
+
+int main()
+{
+  string a = "Hello, World";
+
+  int strLen = stringLength(a);
+  printf("Length of %s is %i", a, strLen);
+  return 0;
+}
